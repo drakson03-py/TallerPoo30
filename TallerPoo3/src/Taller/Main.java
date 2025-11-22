a1c4177210a639e38468bd1f7081b2982de07876
acast <acast@PCGamer>
2025-11-21 21:11:19 -0300

    1.0.4 -menu usuario se agrego actualizar estado de una taera -menu admin se agrego la funcion de eliminar y/o agregar un proyecto -manu admin se agrego la funcion de agregar o eliminar una tarea

--- TallerPoo3/src/Taller/Main.java
+++ TallerPoo3/src/Taller/Main.java
@@ -14,7 +14,7 @@
 		public static String poder;
 		public static String usuario;
 		
-		public static void main(String[] args) throws FileNotFoundException {
+		public static void main(String[] args) throws IOException {
 			
 			Scanner sc = new Scanner(System.in);
 			
@@ -32,8 +32,8 @@
 			
 			
 			String eleccion = sc.nextLine();
-			admin(eleccion);
-			usuario(eleccion);
+			admin(eleccion,sc);
+			usuario(eleccion,sc);
 			
 			
 			
@@ -42,24 +42,26 @@
 			sc.close();
 		}
 		
-		public static void admin(String eleccion) {
+		public static void admin(String eleccion,Scanner sc) throws IOException {
 			if(poder.equals("Administrador")) {
 				
 			switch(eleccion) {
 			
 				case"1":{VerListaCompleta();}
-			
+				case"2":{agregarOEliminarProyecto(sc);}
+				case"3":{agregarOEliminarTarea(sc);}
 			}
 			
 		}
 		}
-		public static void usuario(String eleccion) {
+		public static void usuario(String eleccion,Scanner sc) throws IOException	 {
 			if(poder.equals("Colaborador")) {
 				
 				switch(eleccion) {
 				
 				case"1":{VerLista();}
 				case"2":{tareasAsignadas();}
+				case"3":{cambiarEstadoTarea(sc);}
 				 	
 				}
 				
@@ -183,5 +185,182 @@
 		}
 		
 		public static void print(String txt) {System.out.println(txt);}
+		
+		public static void agregarOEliminarProyecto(Scanner sc) throws IOException {
+
+		    System.out.println("1. Agregar proyecto");
+		    System.out.println("2. Eliminar proyecto");
+		    String opcion = sc.nextLine();
+
+		    if (opcion.equals("1")) {
+		        // AGREGAR
+		        System.out.println("ID del proyecto:");
+		        String id = sc.nextLine();
+
+		        System.out.println("Nombre del proyecto:");
+		        String nombre = sc.nextLine();
+
+		        System.out.println("Responsable:");
+		        String responsable = sc.nextLine();
+
+		        FileWriter fw = new FileWriter("Archivos/proyectos.txt", true);
+		        fw.write(id + "|" + nombre + "|" + responsable + "\n");
+		        fw.close();
+
+		        System.out.println("Proyecto agregado.");
+
+		    } else if (opcion.equals("2")) {
+
+		        System.out.println("ID del proyecto a eliminar:");
+		        String idEliminar = sc.nextLine();
+
+		        File archivoPro = new File("Archivos/proyectos.txt");
+		        Scanner lectorPro = new Scanner(archivoPro);
+
+		        List<String> nuevosProyectos = new ArrayList<>();
+
+		        while (lectorPro.hasNextLine()) {
+		            String linea = lectorPro.nextLine();
+		            String parte[] = linea.split("\\|");
+
+		            if (!parte[0].equals(idEliminar)) {
+		                nuevosProyectos.add(linea);
+		            }
+		        }
+		        lectorPro.close();
+
+		        FileWriter fwPro = new FileWriter("Archivos/proyectos.txt");
+		        for (String l : nuevosProyectos) fwPro.write(l + "\n");
+		        fwPro.close();
+
+		        File archivoTar = new File("Archivos/tareas.txt");
+		        Scanner lectorTar = new Scanner(archivoTar);
+
+		        List<String> nuevasTareas = new ArrayList<>();
+
+		        while (lectorTar.hasNextLine()) {
+		            String linea = lectorTar.nextLine();
+		            String parte[] = linea.split("\\|");
+
+		            if (!parte[0].equals(idEliminar)) {
+		                nuevasTareas.add(linea);
+		            }
+		        }
+		        lectorTar.close();
+
+		        FileWriter fwTar = new FileWriter("Archivos/tareas.txt");
+		        for (String l : nuevasTareas) fwTar.write(l + "\n");
+		        fwTar.close();
+
+		        System.out.println("Proyecto y sus tareas asociadas eliminados.");
+		    }
+		}
+		public static void cambiarEstadoTarea(Scanner sc) throws IOException {
+
+		    System.out.println("ID de la tarea a modificar:");
+		    String idTarea = sc.nextLine();
+
+		    System.out.println("Nuevo estado (Pendiente / En progreso / Completada):");
+		    String nuevoEstado = sc.nextLine();
+
+		    File archivo = new File("Archivos/tareas.txt");
+		    Scanner lector = new Scanner(archivo);
+
+		    List<String> nuevasLineas = new ArrayList<>();
+
+		    while (lector.hasNextLine()) {
+		        String linea = lector.nextLine();
+		        String[] parte = linea.split("\\|");
+
+		        if (parte[1].equals(idTarea)) {
+		            parte[4] = nuevoEstado;
+
+		            String nuevaLinea =
+		                parte[0] + "|" + parte[1] + "|" + parte[2] + "|" +
+		                parte[3] + "|" + parte[4] + "|" + parte[5] + "|" +
+		                parte[6] + "|" + parte[7];
+
+		            nuevasLineas.add(nuevaLinea);
+		        } else {
+		            nuevasLineas.add(linea);
+		        }
+		    }
+		    lector.close();
+
+		    FileWriter fw = new FileWriter("Archivos/tareas.txt");
+		    for (String l : nuevasLineas) {
+		        fw.write(l + "\n");
+		    }
+		    fw.close();
+
+		    System.out.println("Estado actualizado.");
+		}
+		public static void agregarOEliminarTarea(Scanner sc) throws IOException {
+
+		    System.out.println("1. Agregar tarea");
+		    System.out.println("2. Eliminar tarea");
+		    String opcion = sc.nextLine();
+
+		    if (opcion.equals("1")) {
+
+		        System.out.println("ID del proyecto:");
+		        String idProyecto = sc.nextLine();
+
+		        System.out.println("ID de la nueva tarea:");
+		        String idTarea = sc.nextLine();
+
+		        System.out.println("Tipo de tarea (Bug, Feature, Documentacion):");
+		        String tipo = sc.nextLine();
+
+		        System.out.println("Descripción:");
+		        String descrip = sc.nextLine();
+
+		        System.out.println("Estado inicial:");
+		        String estado = sc.nextLine();
+
+		        System.out.println("Responsable:");
+		        String responsable = sc.nextLine();
+
+		        System.out.println("Complejidad:");
+		        String complejidad = sc.nextLine();
+
+		        System.out.println("Fecha:");
+		        String fecha = sc.nextLine();
+
+		        FileWriter fw = new FileWriter("Archivos/tareas.txt", true);
+		        fw.write(idProyecto + "|" + idTarea + "|" + tipo + "|" + descrip + "|" + estado + "|" 
+		                + responsable + "|" + complejidad + "|" + fecha + "\n");
+		        fw.close();
+
+		        System.out.println("Tarea agregada.");
+
+		    } else if (opcion.equals("2")) {
+
+		        System.out.println("ID de la tarea a eliminar:");
+		        String idEliminar = sc.nextLine();
+
+		        File archivo = new File("Archivos/tareas.txt");
+		        Scanner lector = new Scanner(archivo);
+
+		        List<String> nuevasLineas = new ArrayList<>();
+
+		        while (lector.hasNextLine()) {
+		            String linea = lector.nextLine();
+		            String[] parte = linea.split("\\|");
+
+		            if (!parte[1].equals(idEliminar)) {
+		                nuevasLineas.add(linea);
+		            }
+		        }
+		        lector.close();
+
+		        FileWriter fw = new FileWriter("Archivos/tareas.txt");
+		        for (String l : nuevasLineas) fw.write(l + "\n");
+		        fw.close();
+
+		        System.out.println("Tarea eliminada.");
+		    }
+		}
+
 	}
 	
